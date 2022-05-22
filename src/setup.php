8638683744f2c2e70d3f077f4b3a06ca69713f3b
ViacheslavKonashkov <slava.konashkov@gmail.com>
2022-05-22 22:08:59 +0300

    Setup DB

--- src/setup.php
+++ src/setup.php
@@ -1,19 +1,51 @@
 <?php
 
-function createStructure($pdo)
+function createStructure(\PDO $pdo): bool|int
 {
     return $pdo->exec(
         '
-            CREATE TABlE IF NOT EXISTS `USER_BASE` (
-                `id` INT AUTO_INCREMENT PRIMARY KEY,
-                `username` VARCHAR (20),
-                `birthday` DATETIME
-            )
+            CREATE TABLE IF NOT EXISTS user_base
+            (
+                id int auto_increment primary key,
+                username varchar(20) null,
+                birthday datetime null
+            );
         '
     );
 }
 
-$pdo = new \PDO($_ENV['DATABASE_URL'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
-$dbName = $_ENV['DB_DATABASE'];
+function randomString(): string
+{
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $str = '';
+    for ($i = 0; $i < random_int(10, 20); $i++) {
+        $str .= $characters[random_int(0, strlen($characters) - 1)];
+    }
 
+    return ucfirst($str);
+}
+
+function randomDate(\DateTime $start, \DateTime $end): DateTime
+{
+    $randomTimestamp = random_int($start->getTimestamp(), $end->getTimestamp());
+    $randomDate = new \DateTime();
+    $randomDate->setTimestamp($randomTimestamp);
+    return $randomDate;
+}
+
+function fillData(\PDO $pdo): void
+{
+    $count = $pdo->query('SELECT count(*) FROM user_base')->fetchColumn();
+    $limit = 40_000_000;
+    $statement = $pdo->prepare('INSERT INTO user_base (username, birthday) VALUES (?, ?)');
+    for ($i = $limit; $i >= $count; --$i) {
+        $statement->execute([
+            randomString(),
+            randomDate(new \DateTime('1920-01-01'), new \DateTime('2022-12-31'))->format('Y-m-d H:i:s')
+        ]);
+    }
+}
+
+$pdo = new \PDO($_ENV['DATABASE_URL'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
 createStructure($pdo);
+fillData($pdo);
